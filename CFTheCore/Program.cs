cc6c0322f0dcb82bd99769529a6ec5ac8b028204
willydavidjr <willydavidjr@yahoo.com>
2017-09-26 10:36:26 +0800

    Update Program.cs

--- CFTheCore/Program.cs
+++ CFTheCore/Program.cs
@@ -16,6 +16,394 @@ namespace CFTheCore
             int swapping = swapNeighbouringDigits(1234);
             int indexing = arrayMinimumAboveBound(new int[] { 1, 4, 10, 5, 2 }, 1);
         }
+        
+        static int lateRide(int n)
+        {
+            var hours = n / 60;
+            var minutes = n % 60;
+
+            return (hours.ToString() + minutes.ToString()).Sum(c => c - 48);
+        }
+
+        static int[] arrayCenter(int[] a)
+        {
+            int n = a.Length;
+            int first = a[0];
+            int second = a[0];
+            for (int i = 1; i < n; i++)
+            {
+                first += a[i];
+                second = Math.Min(first, a[i]);
+            }
+
+            List<int> B = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if ((Math.Abs(n * a[i]) - first) < n * second)
+                {
+                    B.Add(a[i]);
+                }
+            }
+
+            return B.ToArray();
+        }
+
+
+        static bool isInRange(int a, int b, int c)
+        {
+            return (b >= a) && (b <= c);
+        }
+
+
+        static string FirstNonEmpty(params string[] args)
+        {
+            return args.FirstOrDefault(s => !string.IsNullOrEmpty(s)) ?? string.Empty;
+        }
+
+        static string fibAt(int n)
+        {
+            var x = (BigInteger)Fibonacci(n);
+            return BigInteger.Pow(x, n) + "";
+        }
+
+        public static BigInteger Fibonacci(int n)
+        {
+            BigInteger a = 0;
+            BigInteger b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (BigInteger i = 0; i < n; i++)
+            {
+                BigInteger temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+        static BigInteger fncFindTheSeq(int n)
+        {
+            double x = 1 + Math.Sqrt(5);
+            double xx = Math.Pow(x, n);
+            double y = 1 - Math.Sqrt(5);
+            double yy = Math.Pow(y, n);
+            BigInteger b1 = BigInteger.Pow(2, n);
+            double b2 = Math.Pow(2, n);
+            double b3 = (double)b1;
+            double z = (xx - yy) / (Math.Pow(2, n) * Math.Sqrt(5));
+            BigInteger b = (BigInteger)z;
+            return b;
+        }
+
+        //static decimal fncFindBigSeq(int n)
+        //{
+        //    double x = (1 + Math.Sqrt(5));
+        //    decimal xx = Math.Pow(x, n);
+        //    decimal y = (1 - Math.Sqrt(5));
+        //    decimal yy = System.Numerics.BigInteger.Pow((System.Numerics.BigInteger)y, n);
+        //    decimal z = (xx - yy) / (System.Numerics.BigInteger.Pow(2, n) * (System.Numerics.BigInteger)Math.Sqrt(5));
+        //    return z;
+        //}
+
+        static double fncFindFib(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            return fncFindFib(n - 1) + fncFindFib(n - 2);
+        }
+
+        /*
+          var x = (System.Numerics.BigInteger)(1 + Math.Sqrt(5));
+            var xx = System.Numerics.BigInteger.Pow(x, n);
+            var y = (System.Numerics.BigInteger)(1 - Math.Sqrt(5));
+            var yy = System.Numerics.BigInteger.Pow((System.Numerics.BigInteger)y, n);
+            var z = (xx - yy) / (System.Numerics.BigInteger.Pow(2, n) * (System.Numerics.BigInteger)Math.Sqrt(5));
+            return z;
+         */
+
+        static double fncFindTheSequence(int n)
+        {
+            //double i = (Math.Pow(Math.PI, n))/Math.Sqrt(5);
+            //double ii = n/(1 - n - Math.Pow(n, 2));
+            double x = 1 + Math.Sqrt(5);
+            double xx = Math.Pow(x, n);
+            double y = 1 - Math.Sqrt(5);
+            double yy = Math.Pow(y, n);
+            double z = (xx - yy) / (Math.Pow(2, n) * Math.Sqrt(5));
+            //double yyy = yy/(Math.Pow(2, n)*Math.Sqrt(5));
+            return z;
+        }
+
+        private static int differentValuesInMultiplicationTable(int n, int m)
+        {
+            int result = 0;
+            for (int value = 1; value <= n * m; value++)
+            {
+                for (int i = 1; i <= Math.Min(n, m); i++)
+                {
+                    if (value / i <= Math.Max(n, m) && value % i == 0)
+                    {
+                        result++;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static int applesDistribution(int apples, int boxCapacity, int maxResidue)
+        {
+            int result = 0;
+            for (int x = 1; x <= boxCapacity; x++)
+            {
+                if (apples % x <= maxResidue)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+        bool isInformationConsistent(int[][] evidences)
+        {
+            for (int y = 0; y < evidences[0].Length; y++)
+            {
+                bool innocent = false;
+                bool guilty = false;
+                for (int x = 0; x < evidences.Length; x++)
+                {
+                    switch (evidences[x][y])
+                    {
+                        case -1:
+                            innocent = true;
+                            break;
+                        case 1:
+                            guilty = true;
+                            break;
+                    }
+                }
+
+                if (innocent && guilty)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool ortogonalLines(int[] line1, int[] line2)
+        {
+            if (line1[0] * line2[0] + line1[1] * line2[1] == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static bool isIPv4Address(string inputString)
+        {
+            var checkNull = inputString.Split('.').ToArray();
+
+            if (checkNull.Where(c => !string.IsNullOrEmpty(c)).ToArray().Count() < 4)
+                return false;
+
+            if (checkNull.Where(c => c.All(char.IsNumber)).ToArray().Count() < 4)
+                return false;
+
+            if (checkNull.Count() > 4)
+                return false;
+
+            return inputString.Split('.').All(c => Convert.ToInt32(c) <= 255);
+        }
+
+        int extendedFibonacci(long a, long b, long i)
+        {
+            return 0;
+        }
+
+        static long fncFibMath(long n, long a, long b)
+        {
+            if (n == 0)
+                return b;
+            if (n == 1)
+                return a;
+
+            return (fncFibMath(n - 2, a, b) * a) + ((b) * fncFibMath(n - 1, a, b));
+        }
+
+        int differentSymbolsNaive(string s)
+        {
+            return s.ToCharArray().GroupBy(c => c).Count();
+
+        }
+
+        bool isMAC48Address(string inputString)
+        {
+            for (int x = 0; x < inputString.Length; x++)
+            {
+                if (x % 3 == 2)
+                {
+                    if (inputString[x] != '-')
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (!('0' <= inputString[x] && inputString[x] <= '9' || 'A' <= inputString[x] && inputString[x] <= 'F'))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return inputString.Length == 17;
+        }
+
+        static long monkeyBars(int n)
+        {
+            int intCounter = 0;
+            for (int x = 3; x < n; x++)
+            {
+                foreach (var c in CombinationsWithRepition(new int[] { 1, 2, 3 }, x))
+                {
+                    if (c != null)
+                    {
+                        if (c.Sum(z => z - 48) == (decimal)n)
+                            intCounter++;
+                    }
+                }
+            }
+            return intCounter;
+        }
+
+        static IEnumerable<String> CombinationsWithRepition(IEnumerable<int> input, int length)
+        {
+            string[] str = new string[100];
+            if (length <= 0)
+                yield return "";
+            if (str[length] != string.Empty)
+                yield return str[length];
+            else
+            {
+
+                foreach (var i in input)
+                    foreach (var c in CombinationsWithRepition(input, length - 1))
+                        yield return i.ToString() + c;
+            }
+        }
+
+        private static void sum_up_recursive(List<int> numbers, int target, List<int> partial)
+        {
+            int s = 0;
+            foreach (int x in partial) s += x;
+
+            if (s == target)
+                Console.WriteLine("sum(" + string.Join(",", partial.ToArray()) + ")=" + target);
+
+            if (s >= target)
+                return;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                List<int> remaining = new List<int>();
+                int n = numbers[i];
+                for (int j = i + 1; j < numbers.Count; j++) remaining.Add(numbers[j]);
+
+                List<int> partial_rec = new List<int>(partial);
+                partial_rec.Add(n);
+                sum_up_recursive(remaining, target, partial_rec);
+            }
+        }
+
+        static int beautyOfMath(int n, int k)
+        {
+            var result = Math.Round((double)n / k);
+            return (int)Math.Pow(result, n - 1);
+        }
+
+        static int fncBinomial(int x, int y)
+        {
+            //int x, y;
+            var foo = (1 - x * y);
+            var foo1 = (1 - y - x * y);
+            var foo2 = 1 - (2 * x * y);
+
+            return foo / foo1 * foo2;
+            //if (n <= 1)
+            //    return n;
+
+            //return (fncBinomial(n - 1) * fncBinomial(n - 2)) ;
+
+
+        }
+
+        int arrayMode(int[] sequence)
+        {
+            return sequence.GroupBy(c => c).ToDictionary(c => c.Key, c => c.Count())
+                                  .OrderByDescending(c => c.Value).Take(1).SingleOrDefault().Key;
+        }
+
+        bool isTournament(int n, int[] fromV, int[] toV)
+        {
+            //    int[,] intTour = new int[,];
+            //for (int i = 0; i < n; i++) {
+            //    for (int j = 0; j < fromV.Length; j++) {
+            //        if (i == fromV[j] - 1)
+            //            intTour[i][toV[j] - 1] = 1;
+            //    }
+            //}
+
+            //for (int i = 0; i < intTour.Length; i++)
+            //    for (int j = 0; j < intTour.Length; j++)
+            //        if (i != j && intTour[i][j] == intTour[j][i])
+            //            return false;
+            //return true;
+            return true;
+        }
+
+        static int[] fractionMultiplication(int[] a, int[] b)
+        {
+            return new int[] { };
+        }
+
+        static int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW)
+        {
+            if (weight1 + weight2 <= maxW)
+                return value1 + value2;
+            if (weight1 > maxW && weight2 > maxW)
+                return 0;
+            if (value1 > value2)
+            {
+                if (maxW <= value1)
+                    return value1;
+            }
+            else
+            {
+                if (maxW <= value2)
+                    return value2;
+            }
+            return 0;
+        }
+
+        static char fractionComparison(int[] a, int[] b)
+        {
+
+            double x = Convert.ToDouble(a[0]);
+            double y = Convert.ToDouble(a[1]);
+            double xx = Convert.ToDouble(b[0]);
+            double yy = Convert.ToDouble(b[1]);
+
+            if (Convert.ToDecimal(x / y) == Convert.ToDecimal(xx / yy))
+                return '=';
+            if (Convert.ToDecimal(x / y) > Convert.ToDecimal(xx / yy))
+                return '>';
+            if (Convert.ToDecimal(x / y) < Convert.ToDecimal(xx / yy))
+                return '<';
+            return ' ';
+        }
 
         static int arrayMinimumAboveBound(int[] inputArray, int bound)
         {
