8884049829bf01dd4afe2cb2d16e73c9e3f959ff
Willy David Jr <willydavidjr@yahoo.com>
2019-06-13 00:21:30 +0800

    Adding recursion for Quasifactorial.
    Added some description.

--- CFTheCore/Program.cs
+++ CFTheCore/Program.cs
@@ -19,6 +19,21 @@ namespace CFTheCore
 
             int longes = longestSubstring("abcdabcdabcabcabcd", 3);
         }
+        /*
+            The factorial of n is defined as the product of all natural numbers up to and including n - 1 * 2 * 3 * ... * n. 
+            The quasifactorial of n is different in that after each multiplication the result is decreased by one. 
+            So the quasifactorial of n is (...((1 * 2 - 1) * 3 - 1) * ... * n - 1). The quasifactorial of 1 is 1.
+        */
+        
+        static int quasifactorial(int n) {
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 1;
+    
+            return (n * quasifactorial(n - 1)) - 1;
+
+        }
         
         static int maxDivisor(int left, int right, int divisor)
         {
